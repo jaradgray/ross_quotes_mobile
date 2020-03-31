e8cd990eb3d4100a1ca6003e29528c7a38d7e1fa
Jarad Gray <jarad.h.gray@gmail.com>
2020-03-31 17:21:50 -0500

    MainActivity.java: connect toolbar to navigation

--- app/src/main/java/com/jgendeavors/rossquotes/MainActivity.java
+++ app/src/main/java/com/jgendeavors/rossquotes/MainActivity.java
@@ -1,6 +1,10 @@
 package com.jgendeavors.rossquotes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set toolbar as support action bar
+        final Toolbar toolbar = findViewById(R.id.activity_main_toolbar);
+        setSupportActionBar(toolbar);
+
+        // Connect toolbar to NavController
+        final NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(toolbar, navController);
     }
 }
