4918a0e44e4e0b6ef51b3d2c7ac27aa74d6c504b
Jarad Gray <jarad.h.gray@gmail.com>
2020-03-31 17:44:33 -0500

    DashboardFragment.java: implement basic navigation

--- app/src/main/java/com/jgendeavors/rossquotes/DashboardFragment.java
+++ app/src/main/java/com/jgendeavors/rossquotes/DashboardFragment.java
@@ -4,10 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 public class DashboardFragment extends Fragment {
 
@@ -28,5 +31,43 @@ public class DashboardFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_dashboard, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // put any usages of findViewById() here
+
+        // TODO delete
+        Button buttonRecentMessages = view.findViewById(R.id.fragment_dashboard_b_recent_messages);
+        Button buttonContacts = view.findViewById(R.id.fragment_dashboard_b_contacts);
+        Button buttonSettings = view.findViewById(R.id.fragment_dashboard_b_settings);
+
+        // Get MainActivity's NavController
+        final NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment);
+
+        // TODO delete
+        buttonRecentMessages.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Navigate to RecentMessagesFragment
+                navController.navigate(R.id.action_dashboardFragment_to_recentMessagesFragment);
+            }
+        });
+        buttonContacts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Navigate to ContactsFragment
+                navController.navigate(R.id.action_dashboardFragment_to_contactsFragment);
+            }
+        });
+        buttonSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Navigate to SettingsFragment
+                navController.navigate(R.id.action_dashboardFragment_to_settingsFragment);
+            }
+        });
     }
 }
