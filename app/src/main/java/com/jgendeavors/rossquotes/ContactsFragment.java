a4409c29f9a142e181d5a175a98c55ed53fbdd7b
Jarad Gray <jarad.h.gray@gmail.com>
2020-03-31 17:49:04 -0500

    ContactsFragment.java: implement basic navigation

--- app/src/main/java/com/jgendeavors/rossquotes/ContactsFragment.java
+++ app/src/main/java/com/jgendeavors/rossquotes/ContactsFragment.java
@@ -1,6 +1,44 @@
 package com.jgendeavors.rossquotes;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 public class ContactsFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_contacts, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // put any usages of findViewById() here
+
+        // TODO delete
+        Button buttonContactDetails = view.findViewById(R.id.fragment_contacts_b_contact_details);
+
+        // Get MainActivity's NavController
+        final NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment);
+
+        // TODO delete
+        buttonContactDetails.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Navigate to RecentMessagesFragment
+                navController.navigate(R.id.action_contactsFragment_to_contactDetailsFragment);
+            }
+        });
+    }
 }
