7c5500d898324c8c1fb279f1c04e772cc8888a6c
Jarad Gray <jarad.h.gray@gmail.com>
2020-03-31 17:37:42 -0500

    DashboardFragment.java: inflate layout

--- app/src/main/java/com/jgendeavors/rossquotes/DashboardFragment.java
+++ app/src/main/java/com/jgendeavors/rossquotes/DashboardFragment.java
@@ -1,6 +1,32 @@
 package com.jgendeavors.rossquotes;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class DashboardFragment extends Fragment {
+
+    /*
+    public DashboardFragment() {
+
+    }
+
+    public static DashboardFragment newInstance() {
+        DashboardFragment fragment = new DashboardFragment();
+        return fragment;
+    }
+    */
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+    }
 }
