fb5e2b795807964ec64e61b038c4ebfc7008575c
Jarad Gray <jarad.h.gray@gmail.com>
2020-03-31 17:51:18 -0500

    implement RecentMessagesFragment's basic layout

--- app/src/main/java/com/jgendeavors/rossquotes/RecentMessagesFragment.java
+++ app/src/main/java/com/jgendeavors/rossquotes/RecentMessagesFragment.java
@@ -1,6 +1,19 @@
 package com.jgendeavors.rossquotes;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class RecentMessagesFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_recent_messages, container, false);
+    }
 }
