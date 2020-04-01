dc78e8bcbc65c52a8414ddc43443fccf42f6b81a
Jarad Gray <jarad.h.gray@gmail.com>
2020-04-01 15:05:30 -0500

    ContactDetailsFragment.java: create basic layout and initialize RecyclerView

--- app/src/main/java/com/jgendeavors/rossquotes/ContactDetailsFragment.java
+++ app/src/main/java/com/jgendeavors/rossquotes/ContactDetailsFragment.java
@@ -1,6 +1,44 @@
 package com.jgendeavors.rossquotes;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class ContactDetailsFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_contact_details, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // put any usages of findViewById() here
+
+        // Initialize RecyclerView
+        RecyclerView recyclerView = view.findViewById(R.id.fragment_contact_details_rv_quotes);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setHasFixedSize(true);
+
+        final MessageAdapter adapter = new MessageAdapter();
+        recyclerView.setAdapter(adapter);
+
+        // Handle clicks on RecyclerView items by implementing MessageAdapter.OnItemClickListener interface
+        adapter.setOnItemClickListener(new MessageAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                // TODO handle clicks on Message items
+            }
+        });
+    }
 }
