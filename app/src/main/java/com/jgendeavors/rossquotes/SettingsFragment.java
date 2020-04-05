73b321d823773024e952df1fdcac463da335a467
Jarad Gray <jarad.h.gray@gmail.com>
2020-04-05 14:38:51 -0500

    start setting up the settings screen using androidx Preference library

--- app/src/main/java/com/jgendeavors/rossquotes/SettingsFragment.java
+++ app/src/main/java/com/jgendeavors/rossquotes/SettingsFragment.java
@@ -1,19 +1,12 @@
 package com.jgendeavors.rossquotes;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
+import androidx.preference.PreferenceFragmentCompat;
 
-public class SettingsFragment extends Fragment {
-    @Nullable
+public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.settings_main, rootKey);
     }
 }
