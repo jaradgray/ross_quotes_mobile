90a34ecba5dd3ca38a2bbf9020fe7b8199814ebb
Jarad Gray <jarad.h.gray@gmail.com>
2020-04-06 15:45:34 -0500

    SettingsFragment: enable/disable schedule category based on persisted global app enabled pref

--- app/src/main/java/com/jgendeavors/rossquotes/SettingsFragment.java
+++ app/src/main/java/com/jgendeavors/rossquotes/SettingsFragment.java
@@ -1,17 +1,31 @@
 package com.jgendeavors.rossquotes;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
+    // Instance variables
+    SharedPreferences.OnSharedPreferenceChangeListener mPrefsListener;
+
+
+    // Overridden methods
+
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.settings_main, rootKey);
 
         // Set summaries of our custom IntervalDialogPreferences to reflect persisted values
+        // TODO is this necessary? seems to be updating summary without it...
 
         IntervalDialogPreference minIntervalPref = findPreference("PREF_KEY_MIN_INTERVAL");
 
@@ -19,6 +33,32 @@ public class SettingsFragment extends PreferenceFragmentCompat {
             String summary = minIntervalPref.getInterval().replace(',', ' ');
             minIntervalPref.setSummary(summary);
         }
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // This is a safe place to do ViewModel stuff
+        // We want to link the Schedule Category's enabled/disabled state to the persisted global app enabled/disabled preference
+        //  we can do that through the ViewModel
+
+        // Get a reference to the schedule category so we can set its data later
+        final Preference scheduleCategory = findPreference("schedule_category");
+
+        // Request a ViewModel from the Android system
+        SettingsFragmentViewModel viewModel = ViewModelProviders.of(this).get(SettingsFragmentViewModel.class);
+
+        // Observe the ViewModel's LiveData
+        viewModel.isAppEnabled().observe(getViewLifecycleOwner(), new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean isAppEnabled) {
+                // enable/disable schedule category based on global app enable/disable
+                if (scheduleCategory != null) {
+                    scheduleCategory.setEnabled(isAppEnabled);
+                }
+            }
+        });
     }
 
     /**
