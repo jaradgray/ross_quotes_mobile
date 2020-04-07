b2b75c2316d60bd39cb603964e986134f99f1392
Jarad Gray <jarad.h.gray@gmail.com>
2020-04-06 22:44:08 -0500

    RecentMessagesFragment: set up RecyclerView and Adapter

--- app/src/main/java/com/jgendeavors/rossquotes/RecentMessagesFragment.java
+++ app/src/main/java/com/jgendeavors/rossquotes/RecentMessagesFragment.java
@@ -8,6 +8,8 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class RecentMessagesFragment extends Fragment {
     @Nullable
@@ -15,5 +17,24 @@ public class RecentMessagesFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_recent_messages, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // put any usages of findViewById() here
+
+        // Get references to widgets
+        RecyclerView recyclerView = view.findViewById(R.id.fragment_recent_messages_recycler_view);
+
+        // Initialize RecyclerView
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setHasFixedSize(true);
+
+        final ReceivedMessageAdapter adapter = new ReceivedMessageAdapter();
+        recyclerView.setAdapter(adapter);
+
+        
     }
 }
