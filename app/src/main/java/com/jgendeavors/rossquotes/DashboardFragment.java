99730cb5de44fb18ba34de3741af1e90ca7fe7f2
Jarad Gray <jarad.h.gray@gmail.com>
2020-04-13 13:32:21 -0500

    DashboardFragment.java: drive premium indicator visibility by activity's ViewModel

--- app/src/main/java/com/jgendeavors/rossquotes/DashboardFragment.java
+++ app/src/main/java/com/jgendeavors/rossquotes/DashboardFragment.java
@@ -44,16 +44,11 @@ public class DashboardFragment extends Fragment {
         // put any usages of findViewById() here
 
         // Get references to cards
-        View premiumContainer = view.findViewById(R.id.fragment_dashboard_premium_container);
+        final View premiumContainer = view.findViewById(R.id.fragment_dashboard_premium_container);
         TextView tvVersion = view.findViewById(R.id.fragment_dashboard_tv_version);
         final DashboardCardView cardMessages = view.findViewById(R.id.fragment_dashboard_card_messages);
         final DashboardCardView cardContacts = view.findViewById(R.id.fragment_dashboard_card_contacts);
         final DashboardCardView cardSettings = view.findViewById(R.id.fragment_dashboard_card_settings);
-
-        // Set standard/premium container visibilities based on if premium product is purchased
-        boolean isPremiumPurchased = ((MainActivity)getActivity()).isProductPurchased(MainActivity.PRODUCT_ID_PREMIUM);
-        int visibility = isPremiumPurchased ? View.VISIBLE : View.GONE;
-        premiumContainer.setVisibility(visibility);
 
         // Set version TextView's text to version string
         String versionString = getString(R.string.version_format, BuildConfig.VERSION_NAME);
@@ -86,6 +81,19 @@ public class DashboardFragment extends Fragment {
         });
 
         // Bind UI to the ViewModel
+
+        // request the Activity's ViewModel from the Android system
+        MainActivityViewModel activityViewModel = ViewModelProviders.of(requireActivity()).get(MainActivityViewModel.class);
+        // observe the ViewModel's LiveData
+        // isPremiumPurchased
+        activityViewModel.getIsPremiumPurchased().observe(getViewLifecycleOwner(), new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean isPremiumPurchased) {
+                // update visibility of premium indicator
+                int visibility = isPremiumPurchased ? View.VISIBLE : View.GONE;
+                premiumContainer.setVisibility(visibility);
+            }
+        });
 
         // request a ViewModel from the Android system
         DashboardFragmentViewModel viewModel = ViewModelProviders.of(this).get(DashboardFragmentViewModel.class);
