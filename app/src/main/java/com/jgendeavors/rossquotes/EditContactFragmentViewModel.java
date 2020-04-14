836e3b8629cfa65a62e1499521c72f12b05e9332
Jarad Gray <jarad.h.gray@gmail.com>
2020-04-14 17:19:31 -0500

    Contact: manually set id, and enforce reserved ids

--- app/src/main/java/com/jgendeavors/rossquotes/EditContactFragmentViewModel.java
+++ app/src/main/java/com/jgendeavors/rossquotes/EditContactFragmentViewModel.java
@@ -1,6 +1,8 @@
 package com.jgendeavors.rossquotes;
 
 import android.app.Application;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
@@ -34,7 +36,8 @@ public class EditContactFragmentViewModel extends AndroidViewModel {
         if (contactId == EditContactFragment.ARG_VALUE_NO_CONTACT_ID) {
             // Insert new Contact
             // TODO maybe validate by making sure there's a name or picture
-            contact = new Contact(name, imageAbsolutePath, true);
+            int id = getNextContactId();
+            contact = new Contact(id, name, imageAbsolutePath, true);
             mRepository.insert(contact);
         } else {
             // Update existing Contact
@@ -42,9 +45,29 @@ public class EditContactFragmentViewModel extends AndroidViewModel {
             Contact oldContact = mRepository.getContactSync(contactId);
             boolean isEnabled = oldContact.getIsEnabled();
             // update contact
-            contact = new Contact(name, imageAbsolutePath, isEnabled);
-            contact.setId(contactId);
+            contact = new Contact(contactId, name, imageAbsolutePath, isEnabled);
             mRepository.update(contact);
         }
+    }
+
+
+    // Private methods
+
+    /**
+     * Returns a unique integer to be used for Contact IDs.
+     * Based on this SO answer: https://stackoverflow.com/a/41122403
+     *
+     * @return
+     */
+    private int getNextContactId() {
+        // get the last ID to be stored in SharedPreferences and increment it
+        final String PREF_LAST_CONTACT_ID = "PREF_LAST_CONTACT_ID";
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplication());
+        int result = prefs.getInt(PREF_LAST_CONTACT_ID, Contact.FIRST_FREE_CONTACT_ID) + 1;
+        if (result == Integer.MAX_VALUE) result = Contact.FIRST_FREE_CONTACT_ID; // just to be sure!
+        // update SharedPreferences
+        prefs.edit().putInt(PREF_LAST_CONTACT_ID, result).apply();
+
+        return result;
     }
 }
